e71760bad1cd3dd2430e718bae96ead854aec01d
Joe Sutton <joesutton100@gmail.com>
2016-04-21 18:37:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! Do you want the gossip on Joe? Just say BEGIN to get started.')
+            return bot.say('Hi! 👋 Do you want the gossip on Joe? Just say BEGIN to get started.')
                 .then(() => 'speak');
         }
     },
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that. Remember, I only respond to specific keywords!`).then(() => 'speak');
+                    return bot.say(`🚨 I didn't understand that. Remember, I only respond to specific keywords!`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
